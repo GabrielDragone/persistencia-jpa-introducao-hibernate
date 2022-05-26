c3654114c131b4b1d35dd831d4668877e3eec22c
GabrielDragone <gabriel.dragone@live.com>
2022-05-26 20:35:20 -0300

    Implementando Entidade e configuracoes para salvar registro

--- src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -1,2 +1,29 @@
-package br.com.alura.loja.testes;public class CadastroDeProduto {
+package br.com.alura.loja.testes;
+
+import br.com.alura.loja.modelo.Produto;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.math.BigDecimal;
+
+public class CadastroDeProduto {
+
+    public static void main(String[] args) {
+        Produto celular = new Produto();
+        celular.setNome("Xiaomi");
+        celular.setDescricao("Top top");
+        celular.setPreco(new BigDecimal("800"));
+
+        EntityManagerFactory factory = Persistence.
+                createEntityManagerFactory("loja"); //verificar o persistence.xml
+
+        EntityManager em = factory.createEntityManager();
+
+        em.getTransaction().begin();
+        em.persist(celular);
+        em.getTransaction().commit();
+        em.close();
+
+    }
 }
